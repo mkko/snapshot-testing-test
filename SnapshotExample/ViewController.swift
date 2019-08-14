7b4cb937b6675901cca524f875eedaf9d13fddeb
Mikko Välimäki <mkko1373@gmail.com>
2019-08-14 19:40:18 +0300

    Initial project setup

---
+++ SnapshotExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotExample
+//
+//  Created by Mikko Välimäki on 2019-08-14.
+//  Copyright © 2019 Mikko Välimäki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SnapshotExampleTests/SnapshotExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SnapshotExampleTests.swift
+//  SnapshotExampleTests
+//
+//  Created by Mikko Välimäki on 2019-08-14.
+//  Copyright © 2019 Mikko Välimäki. All rights reserved.
+//
+
+import XCTest
+@testable import SnapshotExample
+
+class SnapshotExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
